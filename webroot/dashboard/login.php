89c1292380b0949c48ea04c38dab292d910d9d7b
Lncvrt <lncvrtreal@gmail.com>
2025-03-03 14:35:56 -0700

    Cloudflare Turnstile script tag change

--- webroot/dashboard/login.php
+++ webroot/dashboard/login.php
@@ -20,7 +20,7 @@ if (isset($_COOKIE['session'])) {
     <link rel="icon" href="/assets/logo.png" type="image/png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit&onload=onloadTurnstileCallback"></script>
+    <script src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit&onload=onloadTurnstileCallback" async defer></script>
     <style>
         body {
             background-color: #1f1f1f;
--- webroot/dashboard/register.php
+++ webroot/dashboard/register.php
@@ -19,7 +19,7 @@ if (isset($_COOKIE['session'])) {
     <title>Login Page</title>
     <link rel="icon" href="/assets/logo.png" type="image/png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap">
-    <script src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit&onload=onloadTurnstileCallback"></script>
+    <script src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit&onload=onloadTurnstileCallback" async defer></script>
     <style>
         body {
             background-color: #1f1f1f;
