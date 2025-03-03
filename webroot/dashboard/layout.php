1d7c9e0900a42887c447bb171cc9871b81cc10c2
Lncvrt <lncvrtreal@gmail.com>
2025-03-03 16:46:50 -0700

    Reformat dashboard layout html

--- webroot/dashboard/layout.php
+++ webroot/dashboard/layout.php
@@ -31,35 +31,35 @@ if (isset($role)) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Xytriza's Uploading Service - <?= $title ?></title>
-    <link rel="icon" href="/assets/logo.png" type="image/png">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <link rel="stylesheet" href="/dashboard/assets/main.css?v=<?php echo filemtime(__DIR__ . '/assets/main.css'); ?>">
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <script src="/dashboard/assets/main.js?v=<?php echo filemtime(__DIR__ . '/assets/main.js'); ?>"></script>
-</head>
-<body>
-    <div id="sidebar">
-        <a href="/" class="logo"><img class="sidebar-item" src="/assets/logo.png" alt="Xytriza's Uploading Service" height="40vw" width="40vw"></a>
-        <a href="/dashboard/"><i class="fas fa-home sidebar-item"></i></a>
-        <a href="/dashboard/gallery.php"><i class="fas fa-file-alt sidebar-item" style="margin-left: 20%;"></i></a>
-        <a href="/dashboard/upload.php"><i class="fas fa-upload sidebar-item"></i></a>
-        <a href="/dashboard/settings.php"><i class="fas fa-cog sidebar-item"></i></a>
-        <a href="/dashboard/account.php" style="margin-top: auto;"><i class="fas fa-user-cog sidebar-item"></i></a>
-        <?php
-        if ($role === 1 || $role === 2) {
-            echo '<a href="/dashboard/admin" class="sidebar-item"><i class="fas fa-user-shield"></i></a>';
-        }
-        ?>
-        <i onclick="logout()" class="fas fa-sign-out-alt sidebar-item"></i>
-    </div>
-    <div id="notification-container"></div>
-    <?= $content ?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Xytriza's Uploading Service - <?= $title ?></title>
+        <link rel="icon" href="/assets/logo.png" type="image/png">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+        <link rel="stylesheet" href="/dashboard/assets/main.css?v=<?php echo filemtime(__DIR__ . '/assets/main.css'); ?>">
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="/dashboard/assets/main.js?v=<?php echo filemtime(__DIR__ . '/assets/main.js'); ?>"></script>
+    </head>
+    <body>
+        <div id="sidebar">
+            <a href="/" class="logo"><img class="sidebar-item" src="/assets/logo.png" alt="Xytriza's Uploading Service"
+                    height="40vw" width="40vw"></a>
+            <a href="/dashboard/"><i class="fas fa-home sidebar-item"></i></a>
+            <a href="/dashboard/gallery.php"><i class="fas fa-file-alt sidebar-item" style="margin-left: 20%;"></i></a>
+            <a href="/dashboard/upload.php"><i class="fas fa-upload sidebar-item"></i></a>
+            <a href="/dashboard/settings.php"><i class="fas fa-cog sidebar-item"></i></a>
+            <a href="/dashboard/account.php" style="margin-top: auto;"><i class="fas fa-user-cog sidebar-item"></i></a>
+            <?php
+            if ($role === 1 || $role === 2) {
+                echo '<a href="/dashboard/admin" class="sidebar-item"><i class="fas fa-user-shield"></i></a>';
+            }
+            ?>
+            <i onclick="logout()" class="fas fa-sign-out-alt sidebar-item"></i>
+        </div>
+        <div id="notification-container"></div>
+        <?= $content ?>
+    </body>
 </html>
